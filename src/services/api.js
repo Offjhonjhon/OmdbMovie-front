74288fedbf61e569ff7a06f804a855bf91d552ad
Offjhonjhon <jjvictorac@gmail.com>
2022-08-15 14:55:26 -0300

    fix: https security problem

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 
 const api = {
-    dbUrl: "https://omdbmovie-1.herokuapp.com",
+    // dbUrl: "https://omdbmovie-1.herokuapp.com/",
+    dbUrl: "http://localhost:5500",
     moviesUrl: "http://www.omdbapi.com/?apikey=8e858c66&"
 }
 
